3bcf1f4952929fd3ef2eaf647dc440ab8f21b116
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-12-12 21:38:46 -1000

    fix suggestions batool

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //template_xyczash
 //service_njecpim
 //0Mtkr8IFmq5-KtZG2
+emailjs.init("0Mtkr8IFmq5-KtZG2");
+
+document.getElementById("contact__form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  emailjs.sendForm("service_njecpim", "template_xyczash", "#contact__form");
+});
 
 function contact() {
    console.log ('it worked')
