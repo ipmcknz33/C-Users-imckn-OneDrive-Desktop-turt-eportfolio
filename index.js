fd5120d9cdbdac09789f05a5b310d3ec5bf7f92b
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-12-16 09:14:17 -1000

    fixed suggestion typos,() and .add

--- index.js
+++ index.js
@@ -3,24 +3,23 @@
 //0Mtkr8IFmq5-KtZG2
 
 
-
-function contact(event) {
+  function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('modal__overy--loading');
-    const success = document.querySelector('modal__overlay--success');
-    loading.classList += " modal__overlay--visable";
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList .add (" .modal__overlay--visible");
 
 emailjs 
      .sendForm(
         'service_njecpim',
         'template_xyczash',
         event.target,
-        'user_0Mtkr8IFmq5-KtZG2'
+        '0Mtkr8IFmq5-KtZG2'
    ).then(() => {
-     loading.classList.remove("modal__overlay--visable");
-    success.classList += " modal__overlay--visable";
+     loading.classList.remove(".modal__overlay--visible");
+    success.classList .add (".modal__overlay--visable");
    }).catch(() =>{
-     loading.classList.remove("modal__overlay--visable");
+     loading.classList.remove(".modal__overlay--visible");
      alert(
         "The email service is temporarily unavailible. Please contact me directly on i.p.mcknz33@gmail.com"
      );
