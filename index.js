5f52b7eeb427b128c5d5fb9f9381109e5ef23bfc
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-12-16 11:10:09 -1000

    fix visible, fix syntex still not fully functioning

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
-    loading.classList .add (" .modal__overlay--visible");
+    loading.classList.add(" .modal__overlay--visible");
 
 emailjs 
      .sendForm(
@@ -17,9 +17,9 @@ emailjs
         '0Mtkr8IFmq5-KtZG2'
    ).then(() => {
      loading.classList.remove(".modal__overlay--visible");
-    success.classList .add (".modal__overlay--visable");
+    success.classList.add(".modal__overlay--visable");
    }).catch(() =>{
-     loading.classList.remove(".modal__overlay--visible");
+     loading.classList.remove(" .modal__overlay--visible");
      alert(
         "The email service is temporarily unavailible. Please contact me directly on i.p.mcknz33@gmail.com"
      );
