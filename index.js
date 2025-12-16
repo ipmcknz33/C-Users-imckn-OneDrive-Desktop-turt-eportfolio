e6e710eb52833803268611dbac17bdaa9d1e8c5b
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-12-16 06:43:51 -1000

    emailjs is hooked up but not responding again.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 //template_xyczash
 //service_njecpim
 //0Mtkr8IFmq5-KtZG2
-emailjs.init("0Mtkr8IFmq5-KtZG2");
 
-document.getElementById("contact__form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  emailjs.sendForm("service_njecpim", "template_xyczash", "#contact__form");
-});
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init({
+        publicKey: "0Mtkr8IFmq5-KtZG2",
+      });
+   })();
+</script>
 
-function contact() {
-   console.log ('it worked')
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('modal__overy--loading');
+    const success = document.querySelector('modal__overlay--success');
+    loading.classList += " modal__overlay--visable";
+
+emailjs 
+     .sendForm(
+        'service_njecpim',
+        'template_xyczash',
+        event.target,
+        'user_0Mtkr8IFmq5-KtZG2'
+   ).then(() => {
+     loading.classList.remove("modal__overlay--visable");
+    success.classList += " modal__overlay--visable";
+   }).catch(() =>{
+     loading.classList.remove("modal__overlay--visable");
+     alert(
+        "The email service is temporarily unavailible. Please contact me directly on i.p.mcknz33@gmail.com"
+     );
+   })
 }
+
+
