ec3f9354bb80533396c1422617521080c210ef29
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-12-17 14:53:47 -1000

    all is built need better responsive at 480

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 //template_xyczash
 //service_njecpim
 //0Mtkr8IFmq5-KtZG2
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const BoolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * BoolInt}px, ${y * BoolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += "dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 const loading = document.querySelector('.modal__overlay--loading');
 const success = document.querySelector('.modal__overlay--success');
 
@@ -17,7 +44,7 @@ emailjs
         '0Mtkr8IFmq5-KtZG2'
    ).then(() => {
      loading.classList.remove("modal__overlay--visible");
-    success.classList.add("modal__overlay--visable");
+    success.classList.add("modal__overlay--visible");
    }).catch(() =>{
      loading.classList.remove("modal__overlay--visible");
      alert(
@@ -26,4 +53,11 @@ emailjs
    })
 }
 
-
+function toggleModal() { 
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;  
+    document.body.classList += " modal--open";
+}
